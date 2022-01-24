9923f1c5fb60249144b824ab13bfa2baf68ffd28
ruskevych <33175520+ruskevych@users.noreply.github.com>
2022-01-24 17:15:01 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,6 @@ class Home extends React.Component {
   
 
   componentDidMount(){
-
-
-   
-    $( "#delete-product-btn" ).click(function() {
-        checkboxes = document.getElementsByClassName('delete-checkbox');
-        for (let item of checkboxes){     
-          if(item.checked){
-            item.click();
-            item.click();
-
-          }
-        }     
-    });
   }
 
   render(){
